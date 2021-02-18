a906d9dd0d4f9118797f80a6973700baddbf686a
Matthew Meyer <ner_nu_anar@yahoo.com>
2021-02-18 15:52:58 -0600

    feat(Tableau): render reset points button when points > 0

--- src/components/Tableau/Tableau.js
+++ src/components/Tableau/Tableau.js
@@ -3,8 +3,6 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import NumberCard from "../NumberCard";
 import Equation from "../Equation";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Badge from "@material-ui/core/Badge";
@@ -196,9 +194,14 @@ const Tableau = () => {
       <Typography variant="button" gutterBottom>
         Total points: {points}
       </Typography>
-      <Button size="small" variant="outlined" onClick={handleResetPoints}>
+      {points > 0 && (<Button
+        size="small"
+        variant="outlined"
+        onClick={handleResetPoints}
+      >
         Reset points
       </Button>
+      )}
     </div>
   );
 };
