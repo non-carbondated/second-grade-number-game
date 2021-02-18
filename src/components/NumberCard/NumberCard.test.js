e8785d8ae3d958c4d2cbba7ef7d2ae91398d1293
Matt Meyer <ner_nu_anar@yahoo.com>
2021-02-18 02:47:05 -0600

    fix(NumberCard): remove unnecessary slash in regex

--- src/components/NumberCard/NumberCard.test.js
+++ src/components/NumberCard/NumberCard.test.js
@@ -24,64 +24,70 @@ describe("NumberCard", () => {
     expect(
       screen.getByRole("button", { name: "Subtract" })
     ).toBeInTheDocument();
-    expect(screen.queryByText(/^\-$/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/^-$/)).not.toBeInTheDocument();
     expect(screen.queryByText(/^\+$/)).not.toBeInTheDocument();
   });
 
   test("renders a minus sign before the number when the subtract button is clicked", () => {
     renderNumberCard(testingProps);
 
-    userEvent.click(screen.getByRole('button', { name: 'Subtract' }))
+    userEvent.click(screen.getByRole("button", { name: "Subtract" }));
 
-    expect(screen.getByText(/^\-$/)).toBeInTheDocument()
+    expect(screen.getByText(/^-$/)).toBeInTheDocument();
     expect(screen.queryByText(/^\+$/)).not.toBeInTheDocument();
-  })
+  });
 
   test("renders a plus sign before the number when the add button is clicked", () => {
     renderNumberCard(testingProps);
 
-    userEvent.click(screen.getByRole('button', { name: 'Add' }))
+    userEvent.click(screen.getByRole("button", { name: "Add" }));
 
-    expect(screen.getByText(/^\+$/)).toBeInTheDocument()
-    expect(screen.queryByText(/^\-$/)).not.toBeInTheDocument();
-  })
+    expect(screen.getByText(/^\+$/)).toBeInTheDocument();
+    expect(screen.queryByText(/^-$/)).not.toBeInTheDocument();
+  });
 
-  test('replaces the Add and Subtract buttons with a single Undo button when Add button is clicked', () => {
-    renderNumberCard(testingProps)
+  test("replaces the Add and Subtract buttons with a single Undo button when Add button is clicked", () => {
+    renderNumberCard(testingProps);
 
-    userEvent.click(screen.getByRole('button', { name: 'Add' }))
+    userEvent.click(screen.getByRole("button", { name: "Add" }));
 
     expect(screen.getByRole("button", { name: "Undo" })).toBeInTheDocument();
-    expect(screen.queryByRole("button", { name: "Add" })).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Add" })
+    ).not.toBeInTheDocument();
     expect(
       screen.queryByRole("button", { name: "Subtract" })
     ).not.toBeInTheDocument();
-  })
+  });
 
-  test('replaces the Add and Subtract buttons with a single Undo button when Subtract button is clicked', () => {
-    renderNumberCard(testingProps)
+  test("replaces the Add and Subtract buttons with a single Undo button when Subtract button is clicked", () => {
+    renderNumberCard(testingProps);
 
-    userEvent.click(screen.getByRole('button', { name: 'Subtract' }))
+    userEvent.click(screen.getByRole("button", { name: "Subtract" }));
 
     expect(screen.getByRole("button", { name: "Undo" })).toBeInTheDocument();
-    expect(screen.queryByRole("button", { name: "Add" })).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Add" })
+    ).not.toBeInTheDocument();
     expect(
       screen.queryByRole("button", { name: "Subtract" })
     ).not.toBeInTheDocument();
-  })
+  });
 
-  test('reverts number card to initial state when the Undo button is clicked', () => {
-    renderNumberCard(testingProps)
+  test("reverts number card to initial state when the Undo button is clicked", () => {
+    renderNumberCard(testingProps);
 
-    userEvent.click(screen.getByRole('button', { name: 'Add' }))
-    userEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    userEvent.click(screen.getByRole("button", { name: "Add" }));
+    userEvent.click(screen.getByRole("button", { name: "Undo" }));
 
     expect(screen.getByRole("button", { name: "Add" })).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: "Subtract" })
     ).toBeInTheDocument();
-    expect(screen.queryByRole("button", { name: "Undo" })).not.toBeInTheDocument();
-    expect(screen.queryByText(/^\-$/)).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Undo" })
+    ).not.toBeInTheDocument();
+    expect(screen.queryByText(/^-$/)).not.toBeInTheDocument();
     expect(screen.queryByText(/^\+$/)).not.toBeInTheDocument();
-  })
+  });
 });
